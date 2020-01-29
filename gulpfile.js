fb1c9f9742c1812c102cb9a264b432f0b4ab9929
Henrik Stavnem <git@dukendor.com>
2020-01-29 15:03:37 +0100

    Compress css, clean-up

--- gulpfile.js
+++ gulpfile.js
@@ -4,22 +4,22 @@ const del = require('del');
 const concat = require('gulp-concat');
 
 gulp.task('styles', () => {
-    return gulp.src('src/scss/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(concat('style.css'))
-        .pipe(gulp.dest('./css/'));
+	return gulp.src('src/scss/**/*.scss')
+		.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+		.pipe(concat('style.css'))
+		.pipe(gulp.dest('./dist/css/'));
 });
 
 gulp.task('clean', () => {
-    return del([
-        'css/main.css',
-    ]);
+	return del([
+		'css/main.css',
+	]);
 });
 
 gulp.task('watch', () => {
-  gulp.watch('src/scss/**/*.scss', (done) => {
-      gulp.series(['clean', 'styles'])(done);
-  });
+	gulp.watch('src/scss/**/*.scss', (done) => {
+	gulp.series(['clean', 'styles'])(done);
+	});
 });
 
 gulp.task('default', gulp.series(['watch', 'clean', 'styles']));
